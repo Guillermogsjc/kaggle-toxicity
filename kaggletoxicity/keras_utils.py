61d6acc4ad04d23b28b16ba6e19fac70deb03a17
guillermogsjc <guillermogsjc@gmail.com>
2018-02-17 19:18:08 +0100

    bidirectional GRU with roc_auc callback

--- kaggletoxicity/keras_utils.py
+++ kaggletoxicity/keras_utils.py
@@ -1,13 +1,11 @@
-#%% SNNNs: es necesario Keras 2.0.5
-from keras.layers import Input, Dense, merge, Reshape, Dropout, BatchNormalization
+# %% SNNNs: es necesario Keras 2.0.5+
+from keras.layers import Input, Dense
 from keras.models import Model
 from keras.constraints import maxnorm
-from keras.regularizers import  l1_l2
 import numpy as np
-from sklearn.metrics import roc_auc_score, f1_score
 from keras.layers.noise import AlphaDropout
-from keras.initializers import TruncatedNormal, RandomUniform
-from keras.initializers import lecun_normal
+from keras.callbacks import Callback
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 
 def SNNN_binary_sig(input_shape_,
@@ -15,9 +13,7 @@ def SNNN_binary_sig(input_shape_,
                     lista_dropouts,
                     maxn,
                     optimizer_='Adam', lr=0.001):
-    
     """ SNNNs para clasificacion 0-1 con salida sigmoide"""
-    
 
     main_input = Input(shape=(input_shape_,), name='main_input')
 
@@ -33,7 +29,6 @@ def SNNN_binary_sig(input_shape_,
                     kernel_constraint=maxnorm(maxn),
                     kernel_initializer='lecun_normal')(x)
         x = AlphaDropout(lista_dropouts[i + 1])(x_d)
-    
 
     main_output = Dense(1, activation='sigmoid', name='main_output')(x)
 
@@ -44,14 +39,48 @@ def SNNN_binary_sig(input_shape_,
     return model
 
 
+class KaggleToxicityValMetric(Callback):
+    def on_train_begin(self, logs={}):
+        self.roc_aucs = []
+        self.pr_aucs = []
+        self.losses = []
+        self.params['metrics'].append('val_roc_auc')
+        logs['val_roc_auc'] = 0
+
+    def on_train_end(self, logs={}):
+        return
+
+    def on_epoch_begin(self, epoch, logs={}):
+        return
+
+    def on_epoch_end(self, epoch, logs={}):
+        logs = logs or {}
+        self.losses.append(logs.get('loss'))
+
+        Y_pred = self.model.predict(self.validation_data[0])
+        n_cols = Y_pred.shape[1]
+
+        roc_auc_list = []
+        for i in range(n_cols):
+            y_true = self.validation_data[1][:, i]
+            y_pred = Y_pred[:, i]
+            roc_auc_epoch = roc_auc_score(y_true.ravel(), y_pred.ravel(), average='weighted')
+            roc_auc_list.append(roc_auc_epoch)
+        roc_auc_mean_epoch = np.mean(roc_auc_list)
+
+        logs['val_roc_auc'] = roc_auc_mean_epoch
+        self.roc_aucs.append(roc_auc_mean_epoch)
+
+        return
+
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        return
 
 
-#%% Callback con roc_auc y pr_auc
-
-from keras.callbacks import Callback
-from sklearn.metrics import roc_auc_score, average_precision_score
-
-
+# %% Callback con roc_auc y pr_auc
 class ExtraValMetric(Callback):
     def on_train_begin(self, logs={}):
         self.roc_aucs = []
@@ -71,7 +100,7 @@ class ExtraValMetric(Callback):
     def on_epoch_end(self, epoch, logs={}):
         logs = logs or {}
         self.losses.append(logs.get('loss'))
-        
+
         y_pred = self.model.predict(self.validation_data[0])
         roc_auc_epoch = roc_auc_score(self.validation_data[1].ravel(), y_pred.ravel(), average='weighted')
         pr_auc_epoch = average_precision_score(self.validation_data[1].ravel(), y_pred.ravel(), average='weighted')
@@ -81,7 +110,7 @@ class ExtraValMetric(Callback):
         logs['val_roc_auc'] = roc_auc_epoch
         logs['val_pr_auc'] = pr_auc_epoch
         self.pr_aucs.append(pr_auc_epoch)
-        
+
         return
 
     def on_batch_begin(self, batch, logs={}):
@@ -89,8 +118,3 @@ class ExtraValMetric(Callback):
 
     def on_batch_end(self, batch, logs={}):
         return
-
-
-
-
-
